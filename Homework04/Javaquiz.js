ee22aa4b46377907db0e95b85ec3749817f74612
abdul Barre <abarre2009@gmail.com>
2019-10-14 18:07:22 -0500

    first commit after xcode error was fixed

---
+++ Homework04/Javaquiz.js
@@ -0,0 +1,24 @@
+var questionsarr = [ 
+{
+  "question": "what state do you want move to?",
+  "options":["firstoption", "second option", "third option", "fourth option"] , 
+  "answer" : "second option"
+},
+{
+  "question": "how long have you lived in your current state?",
+  "options":["firstoption", "second option", "third option", "fourth option"] , 
+  "answer" : "second option"
+
+},
+{
+  "question": "where are you from",
+  "options":["firstoption", "second option", "third option", "fourth option"] , 
+  "answer" : "second option"
+
+},
+{
+  "question": "what year was I born?",
+  "options":["firstoption", "second option", "third option", "fourth option"] , 
+  "answer" : "second option"
+
+}]
