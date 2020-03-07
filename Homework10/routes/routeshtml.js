bef1262080e712c575182cca67fff3d44a1e5bef
abdul Barre <abarre2009@gmail.com>
2020-03-07 13:52:39 -0600

    fixed errors

--- Homework10/routes/routeshtml.js
+++ Homework10/routes/routeshtml.js
@@ -1,13 +1,11 @@
 var router = require("express").Router();
-
 var path = require("path");
 
+router.get("index", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 router.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
-router.get("/index", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = router;
