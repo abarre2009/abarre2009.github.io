3c096fe4687cd05a113fa9103afbbf16ab06315e
abdul Barre <abarre2009@gmail.com>
2020-03-08 16:33:45 -0500

    added useUnifiedTopology

--- Homework16/server.js
+++ Homework16/server.js
@@ -17,7 +17,8 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
